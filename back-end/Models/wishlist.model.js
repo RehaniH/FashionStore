a3edb70e24893ab6843e7adedba6475f4a321d50
shanesoysa <shanesanjaya@gmail.com>
2020-05-23 03:27:11 +0530

    wishlist onclick

--- back-end/Models/wishlist.model.js
+++ back-end/Models/wishlist.model.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose');
 let WishlistSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+       
     }
     ,
     productname: {
         type: String,
-        required: true
+        
+    }
+    ,
+    productimage: {
+        type: String,
     }
     ,
     price: {
--- front-end/src/components/home.component.js
+++ front-end/src/components/home.component.js
@@ -2,18 +2,28 @@ import React, {Component} from "react";
 import axios from 'axios'
 import '../css/home.css'
 import {Link} from "react-router-dom";
+import AddWishlist from "./wishlist/addWishlist.component.js";
 
 const Product = props =>(
     <div className='col-md-3 details div-border'>
         <Link to={'/home/' + props.product.ref_no}>
+            <div className="img-wrapper">
             <img className='img-center img' src={props.product.product_image} alt={props.product.name}/>
+            <div class="img-overlay">
+           
+            </div>
+            </div>
         </Link>
-        <p className='center'>{props.product.name}</p>
+        <p className='center'>{props.product.name}<AddWishlist product={props.product}></AddWishlist></p>
+        
         <p className={props.product.discount !== undefined ? 'cut' :''}>Rs. {props.product.retail_price.toFixed(2)}</p>
         <p><span className='ref'>#{props.product.ref_no}    </span>
             {props.product.discount !== undefined ? '  Rs. '+ props.product.discount.discount_price.toFixed(2): ' '}
         </p>
+            
             <input value='Add to Cart' className='btn btn-danger img-center'/>
+           
+            
     </div>
 );
 
@@ -50,6 +60,7 @@ class Home extends Component{
                         {this.state.products.map(function (currentProduct, i) {
                             return <Product product={currentProduct} key={i}/>;
                         })}
+                        
                     </div>
                 </div>
             </div>
